d42091455c8d0adc98fb227a418bcc47c5f2b017
mikhailova-klavdia <klavdia.mikhailova.001@student.uni.lu>
2025-04-04 01:47:09 +0200

    fix to rag service

--- src/rag/rag.controller.ts
+++ src/rag/rag.controller.ts
@@ -6,14 +6,12 @@ export class RagController {
   constructor(private readonly ragService: RagService) {}
 
   @Post('query')
-  async query(
-    @Body() body
-    ): Promise<string> {
+  async query(@Body() body): Promise<string> {
     if (!body.query) {
       throw new Error('Query parameter is required');
     }
 
-    const answer = this.ragService.retrieveAndGenerate(body.query)
+    const answer = this.ragService.retrieveAndGenerate(body.query);
 
     return answer;
   }
--- src/rag/rag.service.ts
+++ src/rag/rag.service.ts
@@ -10,30 +10,25 @@ export class RagService {
   constructor(
     private readonly _documentService: DocumentService,
     private readonly _chatService: ChatService,
-    private readonly _embeddingService: EmbeddingService,
+    private readonly _embeddingService: EmbeddingService
   ) {}
 
   async retrieveAndGenerate(query: string): Promise<any> {
     // Step 1: Generate embedding for the query
-    console.log('embedding');
     const queryEmbedding = await this._embeddingService.embed(query);
 
     // Step 2: Retrieve relevant documents (you can implement a similarity search here)
-    console.log('retrieve documents');
     const documents = await this._documentService.getAllDocuments();
 
     // Step 3: Find the most relevant document (simple cosine similarity for baseline)
-    console.log('searching for similar docs');
-    const relevantDocument = this._documentService.findMostRelevantDocuments(
+    const relevantDocuments = this._documentService.findMostRelevantDocuments(
       documents,
-      queryEmbedding,
+      queryEmbedding
     );
 
-    if (process.env.MODEL_NAME == 'all-minilm') {
-      return relevantDocument.map((doc) => ({
-        title: doc.document.title,
-        similarity: doc.similarity,
-      }));
-    }
+    return relevantDocuments.map((doc) => ({
+      title: doc.document.title,
+      similarity: doc.similarity,
+    }));
   }
 }
