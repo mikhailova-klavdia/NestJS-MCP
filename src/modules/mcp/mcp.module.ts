d7987ad89e30f91d49bd7ad72c06bd8fff74215c
mikhailova-klavdia <klavdia.mikhailova.001@student.uni.lu>
2025-05-05 12:01:15 +0200

    fix: removed old mcp logic

--- src/modules/mcp/mcp.module.ts
+++ src/modules/mcp/mcp.module.ts
@@ -3,9 +3,10 @@ import { GitModule } from "../git/git.module";
 import { McpTransportType, McpModule as RekogMcpModule } from "@rekog/mcp-nest";
 import { EchoTool } from "./tools/echo.tool";
 import { GreetingTool } from "./tools/greetings.tool";
+import { CloneRepositoryTool } from "dist/modules/mcp/tools/clone-repo.tool";
 
 @Module({
-  providers: [EchoTool, GreetingTool],
+  providers: [EchoTool, GreetingTool, CloneRepositoryTool],
   imports: [
     RekogMcpModule.forRoot({
       name: "mcp-server",
