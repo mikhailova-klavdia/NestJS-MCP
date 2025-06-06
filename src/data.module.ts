e99485400d6b8caf6ab1372ef39f33e3faf26a7b
mikhailova-klavdia <klavdia.mikhailova.001@student.uni.lu>
2025-06-06 20:06:21 +0200

    feat: refactor TypeOrmModule configuration for improved clarity and enable auto-loading of entities

--- src/data.module.ts
+++ src/data.module.ts
@@ -1,34 +1,36 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
-import { DataSource, DataSourceOptions, ColumnType } from "typeorm";
+import { ColumnType, DataSource, DataSourceOptions } from "typeorm";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      useFactory: async (): Promise<TypeOrmModuleOptions> => {
-        const options: DataSourceOptions = {
-          type: "postgres",
-          host: process.env.POSTGRES_HOST,
-          port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
-          username: process.env.POSTGRES_USER,
-          password: process.env.POSTGRES_PASSWORD,
-          database: process.env.POSTGRES_DB,
-          entities: [__dirname + "/**/*.entity{.ts,.js}"],
-          synchronize: false,
-          dropSchema: true,
-        };
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
 
-        const ds = new DataSource(options);
+    TypeOrmModule.forRootAsync({
+      useFactory: async (): Promise<TypeOrmModuleOptions> => ({
+        type: "postgres",
+        host: process.env.POSTGRES_HOST,
+        port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        autoLoadEntities: true,
+        synchronize: true,
+        dropSchema: true
+      }),
+      dataSourceFactory: async (options) => {
+        const ds = new DataSource(options as DataSourceOptions);
 
         ds.driver.supportedDataTypes.push("vector" as ColumnType);
         ds.driver.withLengthColumnTypes.push("vector" as ColumnType);
 
         await ds.initialize();
         await ds.query(`CREATE EXTENSION IF NOT EXISTS vector;`);
-        await ds.synchronize();
-        return ds.options as TypeOrmModuleOptions;
+
+        return ds;
       },
     }),
   ],
