8798d9714bcd446d8619ff912dddef29ddb1edde
mikhailova-klavdia <klavdia.mikhailova.001@student.uni.lu>
2025-06-06 20:28:14 +0200

    feat: update TypeOrmModule configuration to disable auto-loading of entities and synchronize settings

--- src/data.module.ts
+++ src/data.module.ts
@@ -2,6 +2,9 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
 import { ColumnType, DataSource, DataSourceOptions } from "typeorm";
+import { CodeNodeEntity } from "./modules/identifiers/entities/code-node.entity";
+import { CodeEdgeEntity } from "./modules/identifiers/entities/code-edge.entity";
+import { ProjectEntity } from "./modules/project/project.entity";
 
 @Module({
   imports: [
@@ -17,9 +20,10 @@ import { ColumnType, DataSource, DataSourceOptions } from "typeorm";
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
-        autoLoadEntities: true,
-        synchronize: true,
-        dropSchema: true
+        autoLoadEntities: false,
+        entities: [CodeNodeEntity, CodeEdgeEntity, ProjectEntity],
+        synchronize: false,
+        dropSchema: true,
       }),
       dataSourceFactory: async (options) => {
         const ds = new DataSource(options as DataSourceOptions);
@@ -29,6 +33,7 @@ import { ColumnType, DataSource, DataSourceOptions } from "typeorm";
 
         await ds.initialize();
         await ds.query(`CREATE EXTENSION IF NOT EXISTS vector;`);
+        await ds.synchronize();
 
         return ds;
       },
